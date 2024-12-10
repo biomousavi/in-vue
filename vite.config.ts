2ddedad9dcff97363b2fa6a93fbd5aae4eb6b760
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-12-10 23:08:26 +0330

    build: ts types

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,16 @@ import { resolve } from 'node:path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), dts({ rollupTypes: true, tsconfigPath: './tsconfig.app.json' })],
   build: {
     lib: {
       name: 'in-vue',
       fileName: 'in-vue',
+      formats: ['es'],
       entry: resolve(__dirname, 'lib/main.ts'),
     },
     rollupOptions: {
