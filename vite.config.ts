833d75ec1e6df8f48c2fa9baf0f22de5782c73e1
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-12-10 21:05:27 +0330

    build: config build

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,30 @@
 import { fileURLToPath, URL } from 'node:url'
+import { resolve } from 'node:path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue()],
+  build: {
+    lib: {
+      name: 'in-vue',
+      fileName: 'in-vue',
+      entry: resolve(__dirname, 'lib/main.ts'),
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./lib', import.meta.url)),
     },
   },
 })
